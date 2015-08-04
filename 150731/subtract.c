59cdcbd0a25389fbcae57a65c1f78b47791ccd52
Hansol Jang <zzang.hansol@gmail.com>
2015-08-04 21:14:56 +0900

    8/4(21:14) - 분수뺄셈문제, wrong answer, 접근방법은 맞으나 모든 경우가 안나오는 문제 발생

--- 150731/subtract.c
+++ 150731/subtract.c
@@ -7,8 +7,10 @@ int b, n;
 int anscnt;
 int ans[111][2];
 
-int gcd(int a, int b);
 int get_m(int a, int b, int n);
+void swap(int (*arr)[2], int idx1, int idx2);
+int partition(int (*arr)[2], int left, int right);
+void quickSort(int (*arr)[2], int left, int right);
 
 int main()
 {
@@ -20,7 +22,7 @@ int main()
 		if(b == 0 && n == 0)
 			break;
 
-		for(int a=0; a<20; a++)
+		for(int a=0; a<30; a++)
 		{
 			int m = (int)get_m((int)a, b, n);
 			if(m != -1)
@@ -29,6 +31,8 @@ int main()
 				ans[anscnt++][1] = m;
 			}
 		}
+
+		quickSort(ans, 0, anscnt-1);
 
 		for(int i=0; i<anscnt; i++)
 		{
@@ -40,26 +44,70 @@ int main()
 	return 0;
 }
 
-int gcd(int a, int b)
-{
-	if(b == 0) return a;
-	gcd(b, a % b);
-}
-
 int get_m(int a, int b, int n)
 {
 	long long int t1, t2, t3;
-	for(int m=1;m<100; m++)
+	for(int m=1; m<100; m++)
 	{
 		t1 = m * m * b;
 		t2 = 2 * b * n * m;
 		t3 = a * n * n;
 
-		if(t1 - t2 == -(t3))
+		if(t1 - t2 + t3 == 0)
 		{
 			return m;
 		}
 	}
 
 	return -1;
+}
+
+void swap(int (*arr)[2], int idx1, int idx2)
+{
+	int ta = arr[idx1][0];
+	int tm = arr[idx1][1];
+	arr[idx1][0] = arr[idx2][0];
+	arr[idx1][1] = arr[idx2][1];
+	arr[idx2][0] = ta;
+	arr[idx2][1] = tm;
+}
+
+int partition(int (*arr)[2], int left, int right)
+{
+	double pivot = (double)arr[left][0] / (double)arr[left][1];
+	int low = left + 1;
+	int high = right;
+
+	while(low <= high)
+	{
+		while(pivot >= (double)arr[low][0] / (double)arr[low][1])
+		{
+			low++;
+			if(low > high)
+				break;
+		}
+
+		while(pivot < (double)arr[high][0] / (double)arr[high][1])
+		{
+			high--;
+			if(low > high)
+				break;
+		}
+
+		// printf("low %d high %d\n", low, high);
+		if(low <= high)
+			swap(arr, low, high);
+	}
+	swap(arr, left, high);
+	return high;
+}
+
+void quickSort(int (*arr)[2], int left, int right)
+{
+	if(left <= right)
+	{
+		int pivot = partition(arr, left, right);
+		quickSort(arr, left, pivot - 1);
+		quickSort(arr, pivot + 1, right);
+	}
 }
