1f9adb939cfc6b76f849709e7139d77edcaa1ad1
Hansol Jang <zzang.hansol@gmail.com>
2015-08-05 22:24:28 +0900

    8/5(22:24) - remove trash

--- 150731/lcs.c
+++ 150731/lcs.c
@@ -5,9 +5,7 @@ https://www.acmicpc.net/problem/5582
 #include <memory.h>
 
 #define MAX(a,b) (a) > (b) ? (a) : (b)
-#define MIN(a,b) (a) > (b) ? (b) : (a)
 #define SIZE 4444
-#define INF 987654321
 
 char input1[SIZE] ={};
 char input2[SIZE] = {};
@@ -15,7 +13,6 @@ int cache[33];
 int len1 = -1;
 int len2 = -1;
 int maxlen = -1;
-int len;
 
 int lcs(char al;);
 
@@ -46,22 +43,11 @@ int main()
 	{
 		for(int j=0; j<len2; j++)
 		{
-			// if(input1[i] == input2[j])
-			// {
-				len = lcs(i, j);
-				// printf("%d\n", len);
-				if(maxlen < len)
-				{
-					maxlen = len;
-				}
-			// }
+			maxlen = (MAX(maxlen, lcs(i, j)));
 		}
 	}
 
-	if(maxlen == -1)
-		printf("0\n");
-	else
-		printf("%d\n", maxlen);
+	printf("%d\n", maxlen);
 
 	return 0;
 }
@@ -69,7 +55,6 @@ int main()
 //해당 인덱스에서 시작하는 공통부분수열의 길이 반환
 int lcs(int idx1, int idx2)
 {
-	// printf("---------lcs %d:%c %d:%c \n", idx1,input1[idx1], idx2, input2[idx2]);
 	if((input1[idx1] == 0 && input2[idx2] == 0) || 
 		input1[idx1] != input2[idx2])
 	{
@@ -79,8 +64,7 @@ int lcs(int idx1, int idx2)
 	int& ret = cache[idx1][idx2];
 	if(ret != -1)
 	{
-		// printf("-------caching\n");
-		// return ret;
+		return ret;
 	}
 
 	ret = 0;
