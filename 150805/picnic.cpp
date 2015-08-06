72b8eb4ed66232ae65c899b1d95f0643d6e7084a
Hansol Jang <zzang.hansol@gmail.com>
2015-08-06 10:13:13 +0900

    8/6(10:12) - 전역변수로 답을 증가시키는것에서 답을 리턴하도록 수정

--- 150805/picnic.cpp
+++ 150805/picnic.cpp
@@ -10,7 +10,7 @@ bool mate[11][11];
 bool used[11];
 int ans;
 
-void solve(int order, bool* used);
+int solve(int order, bool* used);
 
 int main()
 {
@@ -32,45 +32,42 @@ int main()
 			mate[b][a] = true;
 		}
 
-		solve(0, used);
-		printf("%d\n", ans);
+		printf("%d\n", solve(0, used));
 	}
 	return 0;
 }
 
 //몇번째 친구쌍을 고를 차례이고, 지금까지 맺어준 친구정보를 배열로 받을 때
 //가능한 경우의 수 리턴 
-void solve(int order, bool* used)
+int solve(int order, bool* used)
 {
 	// printf("-----solve call order : %d\n", order);
 	if(order == N)
 	{
-		ans++;
-		return;
+		return 1;
 	}
 
 	int ret = 0;
 	//이미 짝이 되었음
 	if(used[order] == true)
 	{
-		solve(order + 1, used);
+		ret += solve(order + 1, used);
 	}
 	//새 짝을 찾아야함
 	else
 	{	
 		for(int i=0; i<N; i++)
 		{
-			// printf("used %d : %d\n", i, used[i]);
 			if(mate[order][i] == true && used[i] == false)
 			{
-				// printf("order : %d mate :%d %d\n", order, order, i);
 				used[order] = true;
 				used[i] = true;
-				solve(order + 1, used);
+				ret += solve(order + 1, used);
 				used[i] = false;
 				used[order] = false;	
 			}
 		}
 	}
-	// printf("----end order : %d\n", order);
+
+	return ret;
 }
