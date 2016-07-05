f7d0f2d2d0fffdaec749e25df0fab56d6a7e9578
HansolJang <zzang.hansol@gmail.com>
2016-07-05 18:03:28 +0900

    7/5 - 주석추가

--- 01-Arithmetic/1337.cpp
+++ 01-Arithmetic/1337.cpp
@@ -10,13 +10,16 @@ int main() {
 	scanf("%d", &n);
 	for(int i=0; i<n; i++)
 		scanf("%d", &input[i]);
+	//정렬
 	sort(input, input + n);
+	//diff 배열 설정
 	for(int i=0; i<n; i++) {
 		if(i == 0)
 			diff[0] = 0;
 		else
 			diff[i] = input[i] - input[i - 1];
 	}
+	//구간의 합이 4보다 작은 부분 집합 중 원소의 개수가 가장 많은 것 찾기
 	int minsum = inf;
 	int mincnt = 0;
 	int minidx = 0;
@@ -40,15 +43,16 @@ int main() {
 	}
 	int ans = 0;
 	int anscnt = mincnt;
+	//가운데가 비었을 경우 채워 넣기
 	for(int i=1; i<mincnt; i++) {
 		if(diff[minidx + i] > 1) {
 			ans += diff[minidx + i] - 1;
 			anscnt += diff[minidx + i] - 1;
 		}
 	}
+	//다 채웠을 때도 5개가 안될 경우
 	if(anscnt < 5)
 		ans += 5 - anscnt;
-	// printf("idx:%d sum:%d cnt:%d\n", minidx, minsum, mincnt);
 	printf("%d\n", ans);
 	return 0;
 }
