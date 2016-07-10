26ad9bee4f8ead8ba767e7d0f3330a29daac2415
HansolJang <zzang.hansol@gmail.com>
2016-07-10 14:33:16 +0900

    7/10 - 올바른 배열 수정

--- 01-Arithmetic/1337.cpp
+++ 01-Arithmetic/1337.cpp
@@ -3,8 +3,8 @@
 #include <algorithm>
 using namespace std;
 int input[10001];
-int diff[10001];
 int inf = 1000000000;
+int tmp, maxcnt, idx;
 int main() {
 	int n;
 	scanf("%d", &n);
@@ -12,47 +12,20 @@ int main() {
 		scanf("%d", &input[i]);
 	//정렬
 	sort(input, input + n);
-	//diff 배열 설정
+	//input[i]를 기준으로 하는 가장 긴 연속된 부분배열 찾기
 	for(int i=0; i<n; i++) {
-		if(i == 0)
-			diff[0] = 0;
-		else
-			diff[i] = input[i] - input[i - 1];
-	}
-	//구간의 합이 4보다 작은 부분 집합 중 원소의 개수가 가장 많은 것 찾기
-	int minsum = inf;
-	int mincnt = 0;
-	int minidx = 0;
-	int tsum, tcnt;
-	for(int i=0; i<n; i++) {
-		tcnt = 1;
-		tsum = 0;
-		for(int j=1; j<5; j++) {
-			if(i + j < n) {
-				tsum += diff[i + j];
-				tcnt += 1;
-				if(tsum <= 4) {
-					if(mincnt < tcnt) {
-						mincnt = tcnt;
-						minidx = i;
-						minsum = tsum;
-					}
-				}
+		tmp = 1;
+		idx = i + 1; //범위 안에 있을 경우에만 다음 인덱스로
+		for(int j=1; j<5; j++) { //input[i] ~ input[i]+4 범위 안에 있는지
+			if(idx >= n) //인덱스 범위 검사
+				break;
+			if(input[idx] == input[i] + j) {
+				idx += 1;
+				tmp += 1;
 			}
 		}
+		maxcnt = max(maxcnt, tmp);
 	}
-	int ans = 0;
-	int anscnt = mincnt;
-	//가운데가 비었을 경우 채워 넣기
-	for(int i=1; i<mincnt; i++) {
-		if(diff[minidx + i] > 1) {
-			ans += diff[minidx + i] - 1;
-			anscnt += diff[minidx + i] - 1;
-		}
-	}
-	//다 채웠을 때도 5개가 안될 경우
-	if(anscnt < 5)
-		ans += 5 - anscnt;
-	printf("%d\n", ans);
+	printf("%d\n", 5 - maxcnt < 0 ? 0 : 5 - maxcnt); //연속된 배열이 5보다 길경우에도 답은 0
 	return 0;
 }
