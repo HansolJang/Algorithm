3fe61e59871a5ee69b737fa394f1362bb2c36b1c
HansolJang <zzang.hansol@gmail.com>
2016-07-18 21:40:20 +0900

    7/17 - K진 트리(변수 변경)

--- 01-Arithmetic/11812.cpp
+++ 01-Arithmetic/11812.cpp
@@ -2,24 +2,23 @@
 #include <cstdio>
 using namespace std;
 long long n, x, y;
-int k, q, cntx, cnty;
+int k, q, cnt;
 int main() {
 	scanf("%lld %d %d", &n, &k, &q);
 	for(int i=0; i<q; i++) {
 		scanf("%lld %lld", &x, &y);
-		cntx = 0;
-		cnty = 0;
+		cnt = 0;
 		while(x != y) {
 			if(x > y) {
 				x = (x + k - 2) / k;
-				cntx += 1;
+				cnt += 1;
 			}
 			else {
 				y = (y + k - 2) / k;
-				cnty += 1;
+				cnt += 1;
 			}
 		}
-		printf("%d\n", cntx + cnty);
+		printf("%d\n", cnt);
 	}
 	return 0;
 }
