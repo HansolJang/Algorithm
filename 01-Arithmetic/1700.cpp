7a9e1b3461cc8aa4cfcb87470a59971bd2b5402e
HansolJang <zzang.hansol@gmail.com>
2016-08-16 20:45:08 +0900

    멀티탭 스케줄링(가장 늦게 나오는 것 빼기)

--- 01-Arithmetic/1700.cpp
+++ 01-Arithmetic/1700.cpp
@@ -3,46 +3,42 @@
 #include <cstring>
 using namespace std;
 int n, k, max, maxe, ans = 0;
-int ename[101]; //input
-bool plugged[101] = {0, }; //is plugged
-int check[101];
+int ename[100]; //input
+int plugged[100];
 int main() {
 	scanf("%d %d", &n, &k);
 	for(int i=0; i<k; i++)
 		scanf("%d", &ename[i]);
-	int cnt = 0;
-	for(int i=0; i<k; i++) { //처음에 n개 꽂기
-		if(plugged[ename[i]]) continue;
-		plugged[ename[i]] = true;
-		cnt += 1;
-		if(cnt == n) break;
-	}
-	for(int i=n; i<k; i++) {
-		if(plugged[ename[i]]) continue; //원래 꼽힌거면 건너뛰기
-		memset(check, 0, sizeof(check));
-		for(int j=i+1; j<k; j++) {
-			if(!check[ename[j]])
-				check[ename[j]] = j; //몇번째에 나왔었는지 체크
+	int plug_cnt = 1;
+	plugged[0] = ename[0];
+	for(int i=1; i<k; i++) {
+		bool already_plugged = false;
+		for(int j=0; j<n; j++) {
+			if(plugged[j] == ename[i])
+				already_plugged = true;
 		}
-		max = 0;
-		bool unplugged = false;
-		for(int j=1; j<=101; j++) {
-			if(plugged[j]) {
-				if(!check[j]) { //꽂혀있던 것 중 미래에 안나올 것 찾으면 뽑
-					plugged[j] = false;
-					unplugged = true;
+		if(!already_plugged) {
+			max = 0; maxe = 0;
+			for(int j=0; j<n; j++) {
+				bool check = false;
+				for(int m=i+1; m<k; m++) {
+					if(plugged[j] == ename[m]) {
+						if(max < m) {
+							max = m;
+							maxe = j;
+						}
+						check = true;
+						break;
+					}
+				}
+				if(!check) {
+					maxe = j;
 					break;
 				}
-				if(check[j] > max) { //꽂혀있던 것 중 가장 나중에 나올거 찾기
-					max = check[j];
-					maxe = j;
-				}
 			}
+			plugged[maxe] = ename[i];
+			if(++plug_cnt > n) ans += 1;
 		}
-		if(!unplugged)
-			plugged[maxe] = false;
-		plugged[ename[i]] = true; //현재것 꽂기
-		ans += 1;
 	}
 	printf("%d\n", ans);
 	return 0;
