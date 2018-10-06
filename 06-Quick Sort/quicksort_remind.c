2db477a64f3a3e57a6b46edb6b501dcaa6a7c288
hansoljang <zzang.hansol@gmail.com>
2018-10-07 02:46:44 +0900

    퀵소트 수정

--- 06-Quick Sort/quicksort_remind.c
+++ 06-Quick Sort/quicksort_remind.c
@@ -20,9 +20,11 @@ int main() {
 }
 
 void swap(int* arr, int idx1, int idx2) {
-	int temp = arr[idx1];
-	arr[idx1] = arr[idx2];
-	arr[idx2] = temp;
+	if(idx1 != idx2) {
+		arr[idx1] = arr[idx1] + arr[idx2];
+		arr[idx2] = arr[idx1] - arr[idx2];
+		arr[idx1] = arr[idx1] - arr[idx2];
+	}
 }
 
 int partition(int* arr, int left, int right) {
@@ -33,19 +35,20 @@ int partition(int* arr, int left, int right) {
 	while(low <= high) {
 		while(pivot >= arr[low]) {
 			low++;
-			if(low > high)
-				break;
+			if(low > high) break;
 		}
+
 		while(pivot < arr[high]) {
 			high--;
-			if(low > high)
-				break;
+			if(low > high) break;
 		}
-		if(low <= high)	{
-			swap(arr, low, high);
-		}	
+		if(low < high) swap(arr, low, high);
 	}
 	swap(arr, left, high);
+	for(int i=0; i<10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 	return high;
 }
 
@@ -58,7 +61,7 @@ void quickSort(int* arr, int left, int right) {
 }
 
 bool binarySearch(int num, int left, int right) {
-	if(arr[left] > num || arr[right] < num)	{
+	if(arr[left] > num || arr[right] < num)	
 		return false;
 	if(left > right)
 		return false;
