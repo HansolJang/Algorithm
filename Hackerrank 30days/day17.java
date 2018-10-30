9c7720558e0678cbbc412be7eac4b1c3a02965d3
hansoljang <zzang.hansol@gmail.com>
2018-10-30 22:09:39 +0900

    day1. exception

---
+++ Hackerrank 30days/day17.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Calculator {
+    private int n;
+    private int p;
+    
+    public Calculator(){}
+
+    public int power(int n, int p) throws Exception {
+        if(n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        }
+        int result = 1;
+        for(int i = 0; i < p; i++) {
+            result *= n;
+        }
+        return result;
+    }
+}
+
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
